ace26760d792658068aab1e9661874c73913a7c6
Jay Giametta <joseph.giametta@gmail.com>
2021-05-20 17:16:51 -0500

    Initial Commit

---
+++ AODMS/Models/DailyLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AODMS.Models
+{
+    public class DailyLog
+    {
+        /* Autogenerate Ids. JG */
+        [Key]
+        public int Id { get; set; }
+
+        /* Equivalent to unit/PAS. Can have more than one location per base. JG */
+        [Required]
+        public string Location { get; set; }
+
+        /* Log creation date. JG */
+        [Required]
+        public string Date { get; set; }
+    }
+}
---
+++ AODMS/Pages/DailyLog/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AODMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AODMS.Pages.DailyLog
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Models.DailyLog> DailyLogs { get; set; }
+
+        public async Task OnGet()
+        {
+            DailyLogs = await _db.DailyLog.ToListAsync();
+        }
+    }
+}
